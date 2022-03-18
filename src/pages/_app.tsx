3e013b9072acffb60cdf71abf5fd53b13316cc7b
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-03-19 00:14:58 +0900

    [update] add rtkquery

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-
-
+import { store } from '../store'
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next'
 import {
   VStack, Text, Heading,
 } from '@chakra-ui/react'
+import { Counter } from '../features/counter/Counter'
 
 const Home: NextPage = () => {
   return (
     <VStack p={4}>
       <Heading>Hello, World</Heading>
       <Text fontSize='xl'>Hello, World</Text>
+      <Counter />
     </VStack>
   )
 }
